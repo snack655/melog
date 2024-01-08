3f33d0f2e0db553bfd5f8d6d9e85328b0b1217b2
최민재 <cmjcmj1230@gmail.com>
2024-01-08 17:02:57 +0900

    fix : conflict when merge

--- site.config.js
+++ site.config.js
@@ -18,12 +18,10 @@ const CONFIG = {
   ],
   // blog setting (required)
   blog: {
-<<<<<<< HEAD
     title: "melog",
     description: "welcome to melog!",
     theme: "auto", // ['light', 'dark', 'auto']
   },
-
   // CONFIG configration (required)
   link: "https://me-log.vercel.app",
   since: 2023, // If leave this empty, current year will be used.
@@ -32,17 +30,7 @@ const CONFIG = {
   seo: {
     keywords: ["Blog", "Website", "Notion", "Melog", "Android"],
   },
-=======
-    title: "morethan-log",
-    description: "welcome to morethan-log!",
-  },
 
-  // CONFIG configration (required)
-  link: "https://morethan-log.vercel.app",
-  since: 2022, // If leave this empty, current year will be used.
-  lang: "en-US", // ['en-US', 'zh-CN', 'zh-HK', 'zh-TW', 'ja-JP', 'es-ES', 'ko-KR']
-  ogImageGenerateURL: "https://og-image-korean.vercel.app", // The link to generate OG image, don't end with a slash
->>>>>>> upstream/main
 
   // notion configuration (required)
   notionConfig: {
@@ -71,11 +59,7 @@ const CONFIG = {
   utterances: {
     enable: true,
     config: {
-<<<<<<< HEAD
       repo: "snack655/melog",
-=======
-      repo: process.env.NEXT_PUBLIC_UTTERANCES_REPO || "",
->>>>>>> upstream/main
       "issue-term": "og:title",
       label: "💬 Utterances",
     },
